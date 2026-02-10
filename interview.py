fd5be7144c510f220dfc891cc0869ce3db54aaa7
manasa <manasapatel716@gmail.com>
2026-02-10 21:46:29 +0530

    Fixed interview module for Groq

--- interview.py
+++ interview.py
@@ -12,3 +12,43 @@ def ask_groq(prompt):
     )
 
     return response.choices[0].message.content
+
+
+def generate_question(resume_text, job_description, previous_questions):
+    prompt = f"""
+You are a technical interviewer.
+
+Based on the resume and job description below, generate ONE unique interview question.
+Do not repeat previous questions.
+
+Resume:
+{resume_text}
+
+Job Description:
+{job_description}
+
+Previous Questions:
+{previous_questions}
+
+Generate only one new question.
+"""
+    return ask_groq(prompt)
+
+
+def evaluate_answer(question, answer):
+    prompt = f"""
+You are an interview evaluator.
+
+Question:
+{question}
+
+Candidate Answer:
+{answer}
+
+Provide:
+1. Score out of 10
+2. Strengths
+3. Improvements
+4. Final feedback
+"""
+    return ask_groq(prompt)
